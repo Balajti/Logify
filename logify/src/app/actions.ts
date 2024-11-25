9232dd22e3d8ad9c528e6ebc7e10d20fc563788c
Zsolt Balajti <zsolt.balajti@bitninja.io>
2024-11-25 02:06:12 +0100

    fix: update database connection string for Neon database

--- logify/src/app/actions.ts
+++ logify/src/app/actions.ts
@@ -2,7 +2,7 @@
 import { neon } from "@neondatabase/serverless";
 
 export async function getData() {
-    const sql = neon("postgres://neondb_owner:IF7mBYJ6oaui@ep-nameless-mountain-a2iggdpd-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require");
+    const sql = neon("postgresql://neondb_owner:IF7mBYJ6oaui@ep-nameless-mountain-a2iggdpd.eu-central-1.aws.neon.tech/logify-db?sslmode=require");
     const data = await sql`...`;
     return data;
 }
